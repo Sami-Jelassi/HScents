d36575c360b2c30f941f38a762d28e8378fdf2f9
Sami-Jelassi <samijlassi290999@gmail.com>
2026-05-16 18:43:51 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
+    react({
+      babel: {
+        plugins: [
+          // React Compiler (if you want to use it - optional)
+          ['babel-plugin-react-compiler', {}]
+        ]
+      }
+    })
   ],
+  server: {
+    port: 5050,
+    proxy: {
+      '/api': {
+        target: 'http://54.37.159.225:7000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
